d282df24eb404f3a759b6f84a708cdf4f3aae97f
Paulo Felipe <paulofelipetj@hotmail.com>
2023-07-10 15:25:22 -0300

    first commit

---
+++ kotlin-backend-developer-2023/src/main/kotlin/com/tqi/kotlinbackenddeveloper2023/KotlinBackendDeveloper2023Application.kt
@@ -0,0 +1,11 @@
+package com.tqi.kotlinbackenddeveloper2023
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBackendDeveloper2023Application
+
+fun main(args: Array<String>) {
+	runApplication<KotlinBackendDeveloper2023Application>(*args)
+}
---
+++ kotlin-backend-developer-2023/src/test/kotlin/com/tqi/kotlinbackenddeveloper2023/KotlinBackendDeveloper2023ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tqi.kotlinbackenddeveloper2023
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBackendDeveloper2023ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
